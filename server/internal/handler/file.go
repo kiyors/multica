ea09eabbfbea11c6548d164100f3d3bf942dd688
kiyors <gaurav23617@gmail.com>
2026-07-11 11:54:18 +0530

    fix(api): populate download_url in avatar upload response to prevent zod schema validation failure on frontend

--- server/internal/handler/file.go
+++ server/internal/handler/file.go
@@ -456,9 +456,11 @@ func (h *Handler) UploadFile(w http.ResponseWriter, r *http.Request) {
 		}
 
 		writeJSON(w, http.StatusOK, map[string]string{
-			"id":       "",
-			"url":      link,
-			"filename": header.Filename,
+			"id":           "",
+			"url":          link,
+			"download_url": link,
+			"markdown_url": link,
+			"filename":     header.Filename,
 		})
 		return
 	}
@@ -471,9 +473,11 @@ func (h *Handler) UploadFile(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	writeJSON(w, http.StatusOK, map[string]string{
-		"id":       id.String(),
-		"url":      link,
-		"filename": header.Filename,
+		"id":           id.String(),
+		"url":          link,
+		"download_url": link,
+		"markdown_url": link,
+		"filename":     header.Filename,
 	})
 }
 
